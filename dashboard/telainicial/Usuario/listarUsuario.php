2547874d2263bc3d5a3ccbcd0dbfb53b2af5f1cd
bernardobernardo23 <bernardolimasilva13139@gmail.com>
2025-06-09 14:09:19 -0300

    front tabelas

--- dashboard/telainicial/Usuario/listarUsuario.php
+++ dashboard/telainicial/Usuario/listarUsuario.php
@@ -1,54 +1,64 @@
 <?php
-    session_start();
-    require_once '..\..\..\php\Usuario.php';
+session_start();
+require_once '..\..\..\php\Usuario.php';
 
-    if(!isset($_SESSION['usuario_id'])){
-        header ("Location: ..\..\index.php");
-    }
-    if($_SESSION['setor'] === "TI"){  
-        
-    }else{
-    header ('Location: ..\..\php\validacao.php');
+if (!isset($_SESSION['usuario_id'])) {
+    header("Location: ..\..\index.php");
+}
+if ($_SESSION['setor'] === "TI") {
+
+} else {
+    header('Location: ..\..\php\validacao.php');
 }
 
-    $usuario = new Usuario();
-    $usuario= $usuario->listarUsuarios();
+$usuario = new Usuario();
+$usuario = $usuario->listarUsuarios();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar Usuarios</title>
     <link rel="icon" href="../../img/chesiquimica-logo-png.png" type="image/png">
-    <link rel="stylesheet" href="/gerenciadorti/css/listaUsuarios.css">
-
+    <link rel="stylesheet" href="../../../css/listaUsuario.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@600&display=swap"
+        rel="stylesheet">
 
 </head>
+
 <body>
-    <h1>Usuarios Cadastrados:</h1>
-    <a href="..\..\..\php\validacao.php">Voltar</a>
+    <div class="container">
 
-    <table border="1">
-        <tr>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Setor</th>
-            <th>Excluir</th>
-        </tr>
+        <div class="textos">
+            <h1>Usuarios Cadastrados:</h1>
+            <a class="voltar"href="..\..\..\php\validacao.php">&#8592;</a>
+        </div>
 
-        <?php foreach ($usuario as $usuarios)  {?>
+
+        <table border="1">
             <tr>
-                <td><?php echo $usuarios['id'];?></td>
-                <td><?php echo $usuarios['nome'];?></td>
-                <td><?php echo $usuarios['email'];?></td>
-                <td><?php echo $usuarios['setor'];?></td>
-                <td><a href="detalhesUsuarios.php?id=<?=$usuarios['id']; ?>" >Selecionar</a>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Setor</th>
+                <th>Excluir</th>
             </tr>
 
+            <?php foreach ($usuario as $usuarios) { ?>
+                <tr>
+                    <td><?php echo $usuarios['id']; ?></td>
+                    <td><?php echo $usuarios['nome']; ?></td>
+                    <td><?php echo $usuarios['email']; ?></td>
+                    <td><?php echo $usuarios['setor']; ?></td>
+                    <td><a href="detalhesUsuarios.php?id=<?= $usuarios['id']; ?>">&#9998;</a>
+                </tr>
+
             <?php } ?>
-        
+
+    </div>
 </body>
+
 </html>
